b5f7bd8753264adae8d022d9f3e589eb687d74eb
Fadhil <fadhil@incit.org>
2025-11-15 14:13:33 +0700

    Create filament resource for Product model

---
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
+
+class ProductForm
+{
+  public static function configure(Schema $schema): Schema
+  {
+    return $schema
+      ->components([
+          Section::make()->schema([
+            TextInput::make('name')->label('Product Name'),
+            TextInput::make('sku')->label('SKU')->unique(),
+            TextInput::make('slug')->unique(),
+            TextInput::make('stock')->numeric()->default(0),
+            TextInput::make('price')->numeric()->prefix('Rp'),
+            TextInput::make('weight')->numeric()->suffix('gram')
+          ])
+      ]);
+  }
+}
