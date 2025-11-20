af175b6ecdfff9745ffb179e2e43c1205b1cec67
Fadhil <fadhil@incit.org>
2025-11-20 20:32:56 +0700

    Show the basic product list in catalog page

---
+++ app/Livewire/ProductCatalog.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+
+class ProductCatalog extends Component
+{
+  public function render()
+  {
+    $products = Product::paginate(9);
+    return view('livewire.product-catalog', compact('products'));
+  }
+}
