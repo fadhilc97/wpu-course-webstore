54fc76f83668164161269399f4772a7961dcb27a
Fadhil <fadhil@incit.org>
2025-11-22 17:15:01 +0700

    Show product collections data

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -2,18 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Data\ProductCollectionData;
 use App\Data\ProductData;
 use App\Models\Product;
+use App\Models\Tag;
 use Livewire\Component;
 
 class ProductCatalog extends Component
 {
   public function render()
   {
-    $query = Product::paginate(9);
+    $product_result = Product::paginate(9);
+    $collection_result = Tag::query()->withType('collection')->withCount('products')->get();
 
-    $products = ProductData::collect($query);
+    $products = ProductData::collect($product_result);
+    $collections = ProductCollectionData::collect($collection_result);
     
-    return view('livewire.product-catalog', compact('products'));
+    return view('livewire.product-catalog', compact('products', 'collections'));
   }
 }
---
+++ app/Models/Tag.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Spatie\Tags\Tag as TagsTag;
+
+class Tag extends TagsTag
+{
+    public function products(): MorphToMany {
+      return $this->morphedByMany(Product::class, 'taggable');
+    }
+}
