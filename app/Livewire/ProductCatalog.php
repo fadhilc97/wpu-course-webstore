4825c54d33d42687c1d17da86b3ec00ecc833fbb
Fadhil <fadhil@incit.org>
2025-11-24 20:51:29 +0700

    Implement sort and filters in product catalog page

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -7,15 +7,68 @@ use App\Data\ProductData;
 use App\Models\Product;
 use App\Models\Tag;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ProductCatalog extends Component
 {
+  use WithPagination;
+
+  public $queryString = [
+    'selected_collections' => ['except', []],
+    'search' => ['except', ''],
+    'sort_by' => ['except', 'newest'],
+  ];
+
+  public array $selected_collections = [];
+
+  public string $search = '';
+
+  public string $sort_by = 'newest'; // latest, price_asc, price_desc
+
+  public function applyFilters() {
+    $this->resetPage();
+  }
+
+  public function resetFilters() {
+    $this->selected_collections = [];
+    $this->search = '';
+    $this->sort_by = 'newest';
+    $this->resetPage();
+  }
+
   public function render()
   {
-    $product_result = Product::paginate(9);
+    $product_query = Product::query();
     $collection_result = Tag::query()->withType('collection')->withCount('products')->get();
 
-    $products = ProductData::collect($product_result);
+    if ($this->search) {
+      $product_query->where('name', 'LIKE', "%{$this->search}%");
+    }
+
+    if ($this->selected_collections) {
+      $product_query->whereHas('tags', function($query) {
+        $query->whereIn('id', $this->selected_collections);
+      });
+    }
+
+    if ($this->sort_by) {
+      switch($this->sort_by) {
+        case "latest":
+          $product_query->oldest();
+          break;
+        case "price_asc":
+          $product_query->orderBy('price', 'asc');
+          break;
+        case "price_desc":
+          $product_query->orderBy('price', 'desc');
+          break;
+        default:
+          $product_query->latest();
+          break;
+      }
+    }
+
+    $products = ProductData::collect($product_query->paginate(9));
     $collections = ProductCollectionData::collect($collection_result);
     
     return view('livewire.product-catalog', compact('products', 'collections'));
