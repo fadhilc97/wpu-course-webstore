6a17c22f19f78a57307077521726edacecbea61b
Fadhil <fadhil@incit.org>
2025-12-01 20:55:48 +0700

    Show product detail data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Data\ProductData;
 use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function show(Product $product) {
+      $product = ProductData::fromModel($product, true);
       return view('product.show', compact('product'));
     }
 }
