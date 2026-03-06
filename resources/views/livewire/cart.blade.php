400d82f8a9592534bf0ba6561ec1a7d8fc425a06
Fadhil <fadhilc97@gmail.com>
2026-03-06 22:02:19 +0700

    Show product cover url in cart page

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -7,7 +7,7 @@
           <div class="flex items-center gap-5 pb-5 border-b border-gray-200">
             <div class="relative w-40 h-40 overflow-hidden rounded-xl">
               <img class="object-coversize-full"
-                src="https://images.unsplash.com/photo-1546087513-2a2bc7fb6fa9?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src={{ $item->product()->cover_url }}
                 alt="{{ $item->product()->sku }}">
             </div>
             <div class="flex items-center">
