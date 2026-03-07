6a3d95903cfcdd1ff7f9b1a44b4beb374d33b197
Fadhil <fadhilc97@gmail.com>
2026-03-07 13:40:39 +0700

    Change remove cart item button label

--- resources/views/livewire/cart-item-remove.blade.php
+++ resources/views/livewire/cart-item-remove.blade.php
@@ -1,4 +1,4 @@
-<button type="button" wire:click="removeCartItem" class="flex items-center gap-2 text-red-500 cursor-pointer">Hapus <div wire:loading
+<button type="button" wire:click="removeCartItem" class="flex items-center gap-2 text-red-500 cursor-pointer">Remove <div wire:loading
         class="animate-spin inline-block size-4 border-2 border-current border-t-transparent text-red-500 rounded-full dark:text-red-500"
         role="status" aria-label="loading">
         <span class="sr-only">Loading...</span>
