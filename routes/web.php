e893b2ca5f4af8c36035edc151ca13dc241502dc
Fadhil <fadhilc97@gmail.com>
2026-03-19 11:19:22 +0700

    Fix the home page error

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use App\Livewire\Cart;
 use App\Livewire\ProductCatalog;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Livewire\HomePage;
 
-Route::view('/', 'pages.homepage')->name('home');
+Route::get('/', HomePage::class)->name('home');
 Route::get('/products', ProductCatalog::class)->name('product-catalog');
 Route::get('/products/{product:slug}', [ProductController::class, 'show'])->name('product');
 Route::get('/cart', Cart::class)->name('cart');
